5e3673adf5996fe0da65aa9ab5462c351adcc464
jais_vicky <ce21btech11045@iith.ac.in>
2025-05-17 21:03:32 +0530

    feat : event details route

--- app/events/routes.py
+++ app/events/routes.py
@@ -13,3 +13,13 @@ def get_events(event_type):
             "last_updated": cache["last_updated"]
         })
     return jsonify({"error": "Invalid event type"}), 404
+
+@events_bp.route('/api/events/<event_type>/<id>')
+def get_events_details(event_type, id):
+    if event_type in cache["events"]:
+        filtered = [item for item in cache["events"][event_type] if item.get("id") == id]
+        return jsonify({
+            "data": filtered,
+            "last_updated": cache["last_updated"]
+        })
+    return jsonify({"error": "Invalid event type"}), 404
